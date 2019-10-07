65b2852f5e0dab321c16581cf13fb09ccb8f13bc
Christopher Homberger <christopher.homberger@web.de>
2019-10-07 16:38:13 +0200

    Fixed previous bad cleanup

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -213,7 +213,8 @@ namespace WlanRouter {
             Brush password_placeholder = new VisualBrush(new Label() { Content = KEY_HINT, Foreground = new SolidColorBrush(Colors.Gray) }) { Stretch = Stretch.None, AlignmentX = AlignmentX.Left, Viewbox = new Rect(0.0125, 0, 1, 1) };
             Brush password_hint = new VisualBrush(new Label() { Content = String.Format(KEY_HINT_FORMAT, Convert.ToString(8 - (password_box.Visibility == Visibility.Visible && password_cleartext_box.Visibility != Visibility.Visible ? password_box.Password.Length : password_cleartext_box.Text.Length))), Foreground = new SolidColorBrush(Colors.Red) }) { Stretch = Stretch.None, AlignmentX = AlignmentX.Right, Viewbox = new Rect(0, 0.05, 1, 1) };
             ssid_box.Background = ssid_box.Text.Length == 0 ? ssid_placeholder : default_background;
-            password_box.Background = password_cleartext_box.Background = password_box.Visibility == Visibility.Visible && password_box.Password.Length < 8 ? password_box.Password.Length == 0 ? password_placeholder : password_hint : default_background;
+            var length = password_box.Visibility == Visibility.Visible ? password_box.Password.Length : password_cleartext_box.Text.Length;
+            password_box.Background = password_cleartext_box.Background = length < 8 ? length == 0 ? password_placeholder : password_hint : default_background;
             ctrl_key.IsEnabled = ctrl_key_state == true ? ((password_box.Visibility == Visibility.Visible && password_cleartext_box.Visibility != Visibility.Visible ? password_box.Password.Length >= 8 : password_cleartext_box.Text.Length >= 8) && ssid_box.Text.Length != 0) : true;
         }
 
